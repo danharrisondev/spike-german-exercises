0cf873a8e219a2536547609b7c8d7607f4220f26
Daniel Harrison <harrison92d+orangudan@gmail.com>
2019-01-16 10:44:16 +0000

    Update instructions

--- src/FlashcardApp/Instructions/index.js
+++ src/FlashcardApp/Instructions/index.js
@@ -12,7 +12,7 @@ const Button = styled.button`
 
 const Instructions = ({begin}) => (
     <div>
-        <p>This is a simple flashcard game that will test your knowledge of the German cases. You will be asked a series of questions and you must try to answer them as best you can. Any questions that you get wrong you will be re-asked until there are no questions remaining. This is one simple way to memorise the German cases.</p>
+        <p>This is a simple flashcard game that will test your knowledge of the German articles. You will be asked a series of questions and you must try to answer them as best you can. Any questions that you get wrong you will be asked again until there are no questions remaining. This is one simple way to memorise the numerous German articles.</p>
         <Centered>
             <Button onClick={begin}>Begin</Button>
         </Centered>
