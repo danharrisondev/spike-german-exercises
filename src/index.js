c7bab253fcf4aadf67540db0bdbe2ab86afe8831
Daniel Harrison <harrison92d+orangudan@gmail.com>
2019-01-16 08:52:06 +0000

    Add error message if #minigame element missing

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import FlashcardApp from './FlashcardApp';
 
 const wrapper = document.getElementById('minigame');
-ReactDOM.render(<FlashcardApp />, wrapper);
+
+if (wrapper) {
+    ReactDOM.render(<FlashcardApp />, wrapper);
+} else {
+    console.error('Could not load React German Case Minigame, missing root element #minigame')
+};
