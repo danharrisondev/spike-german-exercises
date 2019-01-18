f0c2f1f5f8eea25c72c0ce57a359141b12217719
Daniel Harrison <harrison92d+orangudan@gmail.com>
2019-01-18 15:48:36 +0000

    Expose game start method

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import FlashcardApp from './FlashcardApp';
 
-const wrapper = document.getElementById('minigame');
-
-if (wrapper) {
-    ReactDOM.render(<FlashcardApp />, wrapper);
-} else {
-    console.error('Could not load React German Case Minigame, missing root element #minigame')
+window.flashcards = {
+    startArticlesGame: (selector) => {
+        const wrapper = document.querySelector(selector);
+        ReactDOM.render(<FlashcardApp />, wrapper);
+    }
 };
