2ab15de70185b2d05704772b1fbeca3064180535
Daniel Harrison <harrison92d+orangudan@gmail.com>
2020-05-19 07:47:35 +0200

    Improve styling on the buttons

--- src/MultipleChoice/Card/index.js
+++ src/MultipleChoice/Card/index.js
@@ -10,8 +10,19 @@ const Container = styled.div`
 `;
 
 const Button = styled.button`
+    appearance: none;
     margin: 0 5px;
     padding: 10px 20px;
+    background: #4cb52a;
+    border: 1px solid #224d1f;
+    border-radius: 2px;
+    font-weight: bold;
+    color: #fff;
+    transition: background 0.3s;
+
+    &:hover {
+        background: #3d9421;
+    }
 `;
 
 const Card = ({question, answer, options, success, fail}) => (
